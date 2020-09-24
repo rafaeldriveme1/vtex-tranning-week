b89c2144d430dd1d9c237abd74a3324124f669a3
rafaelcr123 <rafaelcruz1@hotmail.com>
2020-09-24 14:18:50 -0300

    docs: serviços

---
+++ events-example/node/clients/index.ts
@@ -0,0 +1,8 @@
+import { IOClients } from '@vtex/api'
+import Analytics from './analyticsClient'
+
+export class Clients extends IOClients {
+  public get analytics() {
+    return this.getOrSet('analytics', Analytics)
+  }
+}
