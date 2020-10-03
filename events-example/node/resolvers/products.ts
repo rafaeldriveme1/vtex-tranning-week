c9c4aa69b0629a731222e8e3beeda839f0b738db
rafaelcr123 <rafaelcruz1@hotmail.com>
2020-10-02 23:34:11 -0300

    GraphQL: Utilizando GraphiQL

--- events-example/node/resolvers/products.ts
+++ events-example/node/resolvers/products.ts
@@ -4,8 +4,10 @@ export const productList = async (_: any,{ topN }: { topN: number },
     { clients: { masterdata } }: Context
 ) => masterdata.scrollDocuments({
     dataEntity: constants.COURSE_ENTITY,
-    fields: ['count', 'slug'],
+    fields: ['id', 'email'],
     schema: 'v1',
     size: topN,
-    sort: `count DESC`
-}).then((({data}) => data))
+}).then((({data}) => {
+    console.log("=====>", data)
+    return data;
+}))
